babaea4bd5e5f0d459c9b0e6bf64f5969df1b45b
Ulaş Turan <ulasturan@Ulas-MacBook-Pro.local>
2022-09-26 17:30:09 +0300

    chore(deps): add basePath to the config file.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  publicRuntimeConfig: {
-    // Will be available on both server and client
-    contextPath: process.env.NODE_ENV === 'production' ? '/sakai-react' : '',
-  },
-}
+    reactStrictMode: false,
+    swcMinify: true,
+    basePath: process.env.NODE_ENV === "production" ? "/sakai-react" : "",
+    publicRuntimeConfig: {
+        // Will be available on both server and client
+        contextPath: process.env.NODE_ENV === "production" ? "/sakai-react" : "",
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
